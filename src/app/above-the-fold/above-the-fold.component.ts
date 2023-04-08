dd6323710f87eb703045cb53792a52f65869903b
simon-1337 <simon.besenbaeck@gmail.com>
2023-04-08 16:45:38 +0200

    started with above the fold, header

---
+++ src/app/above-the-fold/above-the-fold.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-above-the-fold',
+  templateUrl: './above-the-fold.component.html',
+  styleUrls: ['./above-the-fold.component.scss']
+})
+export class AboveTheFoldComponent {
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AboveTheFoldComponent } from './above-the-fold/above-the-fold.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboveTheFoldComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
