5a8395d48d61057ed5336846d467a153fe2a7b0d
simon-1337 <simon.besenbaeck@gmail.com>
2023-04-14 11:26:45 +0200

    css contact form and css button to contact in above the fold section

--- src/app/contact-form/contact-form.component.ts
+++ src/app/contact-form/contact-form.component.ts
@@ -9,22 +9,26 @@ export class ContactFormComponent {
 
   @ViewChild('myForm') myForm!: ElementRef;
   @ViewChild('nameField') nameField!: ElementRef;
+  @ViewChild('emailField') emailField!: ElementRef;
   @ViewChild('messageField') messageField!: ElementRef;
   @ViewChild('sendButton') sendButton!: ElementRef;
 
   async sendMail() {
     let nameField = this.nameField.nativeElement;
+    let emailField = this.emailField.nativeElement;
     let messageField = this.messageField.nativeElement;
     let sendButton = this.sendButton.nativeElement;
 
     nameField.disabled = true;
+    emailField.disabled = true;
     messageField.disabled = true;
     sendButton.disabled = true;
 
-// send Animation (evtl mit variable)
+    // send Animation (evtl mit variable)
 
     let fd = new FormData();
     fd.append('name', nameField.value);
+    fd.append('email', emailField.value);
     fd.append('message', messageField.value);
 
     await fetch('https://simon-besenbaeck.developerakademie.net/send_mail/send_mail.php',
@@ -34,9 +38,17 @@ export class ContactFormComponent {
       }
     );
 
-      //Text anzeigen Nachricht gesendet (evtl mit Variable nicht sicher)
+    //Text anzeigen Nachricht gesendet (evtl mit Variable nicht sicher)
+
+    nameField.value = '';
+    emailField.value = '';
+    messageField.value = '';
+
     nameField.disabled = false;
+    emailField.disabled = false;
     messageField.disabled = false;
     sendButton.disabled = false;
+
+
   }
 }
